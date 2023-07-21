aae54d150bcb72d7410bd965f93c2d081e6aee92
Robin Herzog <robin.herzog8@gmail.com>
2023-07-21 14:33:43 +0200

    data privacy

--- src/functions/contexts.ts
+++ src/functions/contexts.ts
@@ -19,8 +19,23 @@ export async function httpContexts(
   return {jsonBody: JSON.stringify(contexts)};
 }
 
+export async function httpDataPrivacy(
+  request: HttpRequest,
+  context: InvocationContext,
+): Promise<HttpResponseInit> {
+  return {
+    body: `Aucune de vos données n'est récoltée ni utilisée. Les informations sont stockées sur votre téléphones et l'utilisation du service tiers de OpenAI est utilisé pour traiter vos demandes formulées au travers du chat.`,
+  };
+}
+
 app.http('contexts', {
   methods: ['GET'],
   authLevel: 'anonymous',
   handler: httpContexts,
+});
+
+app.http('dataPrivacy', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler: httpDataPrivacy,
 });
