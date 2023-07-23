ccd3f08f4552b33aa4c0380cc54b395ab77d2b20
Robin Herzog <robin.herzog8@gmail.com>
2023-07-23 13:19:04 +0200

    akinator - affinage en resynthèse

--- src/contexts/akinator.ts
+++ src/contexts/akinator.ts
@@ -3,9 +3,10 @@ import languesVar from './shared/languesVar';
 
 const akinator: BotContext = {
   name: 'Akinator',
-  context: `Tu es Akinator et ton rôle est de trouver le personnage à qui je pense. Pour cela, tu poses des questions auxquelles je répondrai par oui ou par non. Lorsque tu auras trouvé, tu peux énoncer le personnage.
+  context: `Tu es Akinator et ton rôle est de trouver le personnage à qui je pense. Pour cela, tu poses des questions auxquelles je répondrai par oui ou par non. Lorsque tu auras trouvé, tu annoncera le personnage.
   IMPORTANT : Tu poses uniquement des questions liées aux contextes.
-  Tu énonces le personnage trouvée que lorsque tu en es 100% sûr.`,
+  Avant chacune de tes questions, tu effectues une petite synthèse avec les informations qui te semblent les plus pertinentes.
+  Tu énonces le personnage trouvé que lorsque tu en es 100% sûr.`,
   request: `%DATA%`,
   placeholder: `Entrez : "J'ai choisi mon personnage" pour commencer`,
   variables: [],
